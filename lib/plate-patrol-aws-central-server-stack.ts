ba639884c778760643e2eaa382874dbaf15a1e71
Bingcheng Li <bingchel@andrew.cmu.edu>
2025-03-08 00:47:40 -0400

    Initial commit

---
+++ lib/plate-patrol-aws-central-server-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class PlatePatrolAwsCentralServerStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'PlatePatrolAwsCentralServerQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
