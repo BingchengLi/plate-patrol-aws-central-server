e953b068757edc9cf436e72dc077f654f8fa712a
Bingcheng Li <bingchel@andrew.cmu.edu>
2025-03-08 16:30:43 -0500

    unioque name per stage

--- resources/tables.ts
+++ resources/tables.ts
@@ -4,15 +4,14 @@ import { Construct } from "constructs";
 export class Tables {
   public readonly watchlistTable: dynamodb.Table;
 
-  constructor(scope: Construct) {
-    // Define the Global Watchlist Table
-    this.watchlistTable = new dynamodb.Table(scope, "WatchlistTable", {
-      tableName: "global_watchlist",
+  constructor(scope: Construct, stage: string) {
+    this.watchlistTable = new dynamodb.Table(scope, `WatchlistTable-${stage}`, {
+      tableName: `global_watchlist_${stage}`, // Unique name per stage
       partitionKey: {
         name: "plate_number",
         type: dynamodb.AttributeType.STRING,
       },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, // Auto scaling
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, // Auto-scaling
     });
   }
 }
