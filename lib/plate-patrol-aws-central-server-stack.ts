c75414d7808cd95b73282f7971211ac10cd72a7d
Bingcheng Li <bingchel@andrew.cmu.edu>
2025-03-08 16:30:35 -0500

    added stage in context

--- lib/plate-patrol-aws-central-server-stack.ts
+++ lib/plate-patrol-aws-central-server-stack.ts
@@ -4,21 +4,37 @@ import { Tables } from "../resources/tables";
 import { Lambdas } from "../resources/lambdas";
 
 export class PlatePatrolAwsCentralServerStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+  constructor(
+    scope: cdk.App,
+    id: string,
+    stage: string,
+    props?: cdk.StackProps
+  ) {
     super(scope, id, props);
 
     // Create Tables
-    const tables = new Tables(this);
+    const tables = new Tables(this, stage); // Pass stage name to tables
     const watchlistTable = tables.watchlistTable;
 
     // Create Lambda Functions
     const lambdas = new Lambdas(this, watchlistTable.tableName);
     const detectionsLambda = lambdas.detectionsLambda;
 
-    // Create API Gateway
-    const api = new apigateway.RestApi(this, "PlatePatrolCentralServerAPI", {
-      restApiName: "Plate Patrol Central Server API",
-      description: "APIs for Plate Patrol Central Server",
+    // Create API Gateway **without default `prod`**
+    const api = new apigateway.RestApi(this, `PlatePatrolAPI-${stage}`, {
+      restApiName: `Plate Patrol Central Server API (${stage})`,
+      description: `APIs for Plate Patrol Central Server - Stage: ${stage}`,
+      deploy: false, // Do not create a default stage
+    });
+
+    // Create a Deployment and Associate it with a Custom Stage (dev, staging, prod)
+    const deployment = new apigateway.Deployment(this, `Deployment-${stage}`, {
+      api,
+    });
+
+    api.deploymentStage = new apigateway.Stage(this, `Stage-${stage}`, {
+      deployment,
+      stageName: stage,
     });
 
     // Add `GET /detections/{plate_number}` Endpoint
@@ -27,7 +43,9 @@ export class PlatePatrolAwsCentralServerStack extends cdk.Stack {
       .addResource("{plate_number}")
       .addMethod("GET", new apigateway.LambdaIntegration(detectionsLambda));
 
-    // Output API Gateway URL
-    new cdk.CfnOutput(this, "ApiUrl", { value: api.url });
+    // Output base URL of the API Gateway
+    new cdk.CfnOutput(this, `ApiUrl-${stage}`, {
+      value: api.url,
+    });
   }
 }
