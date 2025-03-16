c18cfa34e1367405bfe0d440401bcf6cc4e81cdc
Bingcheng Li <bingchel@andrew.cmu.edu>
2025-03-15 20:29:29 -0400

    added serrets for ci testing

--- tests/jest/detections.test.ts
+++ tests/jest/detections.test.ts
@@ -4,13 +4,14 @@ import dotenv from "dotenv";
 // Load environment variables
 dotenv.config();
 
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:3000";
+const API_BASE_URL =
+  process.env.API_BASE_URL ||
+  "https://xat4qx9kpj.execute-api.us-east-2.amazonaws.com";
 const STAGE = process.env.STAGE || "dev";
 const API_URL = `${API_BASE_URL}/${STAGE}`;
-console.log("API_URL: ", API_URL);
 
-const VALID_API_KEY = process.env.VALID_API_KEY || "";
-console.log("VALID_API_KEY: ", VALID_API_KEY);
+const VALID_API_KEY =
+  process.env.VALID_API_KEY || "RbC1Fostw07gDZQNEhqYz1UEKySIRKwE7mkMf7Hs";
 const INVALID_API_KEY = "invalid-api-key";
 const TEST_PLATE_NUMBER = "ABC123";
 const TEST_REASON = "Suspicious vehicle";
