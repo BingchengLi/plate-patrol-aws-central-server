e3a3c9544fdae765405d316354530e53788c5ba0
Bingcheng Li <bingchel@andrew.cmu.edu>
2025-04-26 17:07:25 -0400

    finished frontend setup

---
+++ demo-web-app/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UploadMonitor from "./pages/UploadMonitor"; // or whatever your page is
+
+function App() {
+  return (
+    <div>
+      <h1>PlatePatrol Web App Wrapper (Only for demo purposes)</h1>
+      <UploadMonitor />
+    </div>
+  );
+}
+
+export default App;
